ca4eda32212fcd8ef73e70f226364aec6fbcce53
Vince JV <1276544+vincejv@users.noreply.github.com>
2024-11-25 15:59:12 +0800

    Add explicit `/metrics` endpoint `@Produces` content-type

--- src/main/java/qbittorrent/exporter/Application.java
+++ src/main/java/qbittorrent/exporter/Application.java
@@ -4,7 +4,10 @@ import io.vertx.core.http.HttpServerResponse;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import qbittorrent.exporter.handler.QbtHttpHandler;
+
+import static io.prometheus.client.exporter.common.TextFormat.CONTENT_TYPE_OPENMETRICS_100;
 
 @Path("")
 public class Application {
@@ -14,6 +17,7 @@ public class Application {
 
     @GET
     @Path("metrics")
+    @Produces(CONTENT_TYPE_OPENMETRICS_100)
     public void metrics(HttpServerResponse serverResponse) {
         qbtHttpHandler.handleRequest(serverResponse);
     }
